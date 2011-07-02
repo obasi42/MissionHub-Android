85e0c5bede95cf278dc4f240561f3d6631a111d6
Chris Roemmich <chris.roemmich@cojourners.com>
2011-07-02 19:13:26 -0400

    new activities

--- src/com/missionhub/LoginActivity.java
+++ src/com/missionhub/LoginActivity.java
@@ -1,17 +1,13 @@
 package com.missionhub;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MissionHubActivity extends Activity {
+public class LoginActivity extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		 Intent i = new Intent(this, LoginActivity.class);
-         startActivity(i);
+		setContentView(R.layout.login);
 	}
 }
