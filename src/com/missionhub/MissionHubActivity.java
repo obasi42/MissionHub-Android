3d70b24ca54a60382ab03ecb30a63d7d95870891
Chris Roemmich <chris.roemmich@cojourners.com>
2011-07-02 00:24:20 -0400

    Initial Import of Default App

---
+++ src/com/missionhub/MissionHubActivity.java
@@ -0,0 +1,13 @@
+package com.missionhub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MissionHubActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
