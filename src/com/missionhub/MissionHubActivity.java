8f47585a147a19e60eb13c7590b991cb827fc7ff
Chris Roemmich <chris.roemmich@cojourners.com>
2011-07-02 19:31:41 -0400

    add click actions

--- src/com/missionhub/MissionHubActivity.java
+++ src/com/missionhub/MissionHubActivity.java
@@ -3,6 +3,7 @@ package com.missionhub;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MissionHubActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -10,8 +11,18 @@ public class MissionHubActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	}
+	
+	public void clickLogin(View view) {
+		Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+	
+	public void clickAbout(View view) {
 		
-		 Intent i = new Intent(this, LoginActivity.class);
-         startActivity(i);
+	}
+	
+	public void clickContact(View view) {
+		
 	}
 }
