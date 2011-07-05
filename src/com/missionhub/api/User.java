e1c96a2526b1e613337dbb5a4e7ef95450786317
Chris Roemmich <chris.roemmich@cojourners.com>
2011-07-05 18:20:18 -0400

    add contact to user

--- src/com/missionhub/api/User.java
+++ src/com/missionhub/api/User.java
@@ -4,7 +4,7 @@ public class User {
 	
 	public static String token;
 	public static boolean isLoggedIn = false;
-	public static Contact contact;
 	public static String orgID;
+	public static GContact contact;
 	
 }
