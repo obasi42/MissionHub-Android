935ee85609d24cf27a98bb165ad9ed23249c7e39
Matt Webb <mattrw89@gmail.com>
2011-07-05 10:34:11 -0400

    minor changes

--- src/com/missionhub/MissionHubActivity.java
+++ src/com/missionhub/MissionHubActivity.java
@@ -45,7 +45,85 @@ public class MissionHubActivity extends Activity {
 		loggedIn = (LinearLayout) findViewById(R.id.loggedin);
 		
 		refreshView();
+}
 
+	public final int LOGIN_WINDOW_ACTIVITY = 0;
+	public void clickLogin(View view) {
+		Intent i = new Intent(this, LoginActivity.class);
+		startActivityForResult(i, LOGIN_WINDOW_ACTIVITY);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == LOGIN_WINDOW_ACTIVITY && resultCode == RESULT_OK) {
+			Log.i(TAG, data.getStringExtra("token"));
+			refreshView();
+		}
+	}
+	
+	public void refreshView() {
+		if (LoginActivity.token != null && LoginActivity.isLoggedIn) {
+			loggedOut.setVisibility(View.GONE);
+			loggedIn.setVisibility(View.VISIBLE);
+		} else {
+			loggedIn.setVisibility(View.GONE);
+			loggedOut.setVisibility(View.VISIBLE);
+		}
+	}
+
+	public void clickAbout(View view) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(R.string.alert_learn_more)
+				.setIcon(R.drawable.ic_dialog_info)
+				.setMessage(R.string.alert_learn_more_msg)
+				.setPositiveButton(R.string.alert_ok, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						Uri uri = Uri.parse("http://missionhub.com?mobile=0");
+						startActivity(new Intent(Intent.ACTION_VIEW, uri));
+					}
+				})
+				.setNegativeButton(R.string.alert_cancel, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+
+	public void clickContact(View view) {
+
+	}
+
+	public void testingApi() {
+		
+		LoginActivity.token = "43941a348dbb0b6c6e88763338baa5bedc08ddaa3c139106c700b8a45e1e8205";
+		JsonHttpResponseHandler responseHandler = new JsonHttpResponseHandler() {
+
+			@Override
+			public void onStart() {
+				Log.i(TAG2, "Starting");
+			}
+			
+			@Override
+			public void onSuccess(JSONArray object) {
+
+				Log.i(TAG2, "SUCCESS!");
+				Log.i(TAG2, object.toString());
+				
+			}
+			
+			@Override
+			public void onFailure(Throwable e) {
+				Log.i(TAG2, e.toString());
+				Log.i(TAG2, "stuff");
+			}
+			
+			@Override
+			public void onFinish() {
+				Log.i(TAG2, "I'm finished!");
+			}
+		};
+		
 		JsonHttpResponseHandler responseHandler = new JsonHttpResponseHandler() {
 
 			@Override
@@ -96,52 +174,6 @@ public class MissionHubActivity extends Activity {
 		rejoicables.add("gospel_presentation");
 		
 		Api.postFollowupComment(1282204, 244771, "completed", "Hi from the Android API", responseHandler, rejoicables);
-	}
-
-	public final int LOGIN_WINDOW_ACTIVITY = 0;
-	public void clickLogin(View view) {
-		Intent i = new Intent(this, LoginActivity.class);
-		startActivityForResult(i, LOGIN_WINDOW_ACTIVITY);
-	}
 	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == LOGIN_WINDOW_ACTIVITY && resultCode == RESULT_OK) {
-			Log.i(TAG, data.getStringExtra("token"));
-			refreshView();
-		}
-	}
-	
-	public void refreshView() {
-		if (LoginActivity.token != null && LoginActivity.isLoggedIn) {
-			loggedOut.setVisibility(View.GONE);
-			loggedIn.setVisibility(View.VISIBLE);
-		} else {
-			loggedIn.setVisibility(View.GONE);
-			loggedOut.setVisibility(View.VISIBLE);
-		}
-	}
-
-	public void clickAbout(View view) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle(R.string.alert_learn_more)
-				.setIcon(R.drawable.ic_dialog_info)
-				.setMessage(R.string.alert_learn_more_msg)
-				.setPositiveButton(R.string.alert_ok, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						Uri uri = Uri.parse("http://missionhub.com?mobile=0");
-						startActivity(new Intent(Intent.ACTION_VIEW, uri));
-					}
-				})
-				.setNegativeButton(R.string.alert_cancel, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.cancel();
-					}
-				});
-		AlertDialog alert = builder.create();
-		alert.show();
-	}
-
-	public void clickContact(View view) {
-
 	}
 }
