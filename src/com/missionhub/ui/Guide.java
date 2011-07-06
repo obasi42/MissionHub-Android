e7c7c45329086eab4dd2dd8fc35d239a20dc8f4e
Chris Roemmich <chris.roemmich@cojourners.com>
2011-07-06 10:51:34 -0400

    working guide

--- src/com/missionhub/ui/Guide.java
+++ src/com/missionhub/ui/Guide.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 public class Guide {
 	
@@ -13,16 +14,17 @@ public class Guide {
 	public static final int CONTACTS_UNASSIGNED = 1;
 	public static final int CONTACT = 2;
 	
-	public static final String PREFS_NAME = "MissionHubPrivate";
+	public static final String PREFS_NAME = "MissionHubGuide";
 	
 	public static void display(Context ctx, int id) {
-		if (!shouldShow(ctx, id)) return;
+		if (dontShow(ctx, id)) return;
 		
 		final Context context = ctx;
+		final int guideId = id;
 		
 		String title = "";
 		String message = "";
-		switch(id) {
+		switch(guideId) {
 			case CONTACTS_MY_CONTACTS:
 				title = ctx.getString(R.string.guide_contacts);
 				message = ctx.getString(R.string.guide_contact_msg);
@@ -46,22 +48,23 @@ public class Guide {
 						dialog.cancel();
 					}
 				})
-				.setNegativeButton(R.string.alert_close, new DialogInterface.OnClickListener() {
+				.setNegativeButton(R.string.alert_dont_show, new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int id) {
 						dialog.cancel();
-						setNeverShow(context, id);
+						setDontShow(context, guideId);
 					}
 				});
 		AlertDialog ad = builder.create();
 		ad.show();
 	}
 	
-	public static boolean shouldShow(Context ctx, int id) {
+	public static boolean dontShow(Context ctx, int id) {
 		SharedPreferences settings = ctx.getSharedPreferences(PREFS_NAME, 0);
 		return settings.getBoolean("guide_"+id, false);
 	}
 	
-	public static boolean setNeverShow(Context ctx, int id) {
+	public static boolean setDontShow(Context ctx, int id) {
+		Log.i("GUIDE", "Set Never Show: " + id);
 		SharedPreferences settings = ctx.getSharedPreferences(PREFS_NAME, 0);
 		SharedPreferences.Editor editor = settings.edit();
 		editor.putBoolean("guide_"+id, true);
