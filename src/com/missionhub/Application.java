0ea3eb66741bc532f04ee7c37be68eedf835dba4
Chris Roemmich <chris.roemmich@cojourners.com>
2011-07-17 09:43:04 -0400

    added comments to application.java

--- src/com/missionhub/Application.java
+++ src/com/missionhub/Application.java
@@ -11,7 +11,12 @@ public class Application {
 
 	/* Running Application Version */
 	private static String version;
-	
+
+	/**
+	 * Saves the application state to a bundle
+	 * @param b the bundle
+	 * @return the bundle with saved state
+	 */
 	public static Bundle saveApplicationState(Bundle b) {
 		b.putString("_version", version);
 		Auth.saveState(b);
@@ -19,6 +24,10 @@ public class Application {
 		return b;
 	}
 	
+	/**
+	 * Restores the application's state from a bundle
+	 * @param b the bundle
+	 */
 	public static void restoreApplicationState(Bundle b) {
 		if (b == null || b.isEmpty()) return;
 		version = b.getString("_appVersion");
@@ -26,18 +35,30 @@ public class Application {
 		User.restoreState(b);
 	}
 	
+	/**
+	 * Sets the version number for the app from the package manager
+	 * Only needs to be run once, version is saved by saveApplicationState
+	 * @param ctx
+	 */
 	public static synchronized void initVersion(Context ctx) {
 		try {
 			Application.setVersion(String.valueOf(ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0).versionCode));
 		} catch (NameNotFoundException e) {}
 	}
 	
+	/**
+	 * Set the version number
+	 * @param version Application Version
+	 */
 	public static synchronized void setVersion(String version) {
 		Application.version = version;
 	}
 	
+	/**
+	 * Returns the application's version number
+	 * @return
+	 */
 	public static synchronized String getVersion() {
 		return version;
 	}
-	
 }
