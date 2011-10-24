a5eeb7c4341ec33ebf9407930d4c73c5f17a76bb
croemmich <chris.roemmich@cojourners.com>
2011-10-24 03:37:03 -0500

    ugg.

--- src/com/missionhub/config/Config.java
+++ src/com/missionhub/config/Config.java
@@ -1,27 +1,13 @@
 package com.missionhub.config;
 
 public class Config {
-	
-//	public static final String baseUrl = "https://www.missionhub.com";
-//	public static final String cookieHost = "www.missionhub.com";
-//	public static final String oauthUrl = baseUrl + "/oauth";
-//	public static final String apiUrl = baseUrl + "/api";
-//	public static final String apiVersion = "2";
-//	public static final String oauthClientId = "5";
-//	public static final String oauthClientSecret = "a47326052ee47060847acc7a8bbdc587f6ba5dac70d2be82b6a1a4761037b3c8";
-//	public static final String oauthScope = "userinfo,contacts,contact_assignment,followup_comments,roles,organization_info";
-//	public static final String flurryKey = "GNH51RVF8SGEMQZX5DQT"; // Development
-//	//public static final String flurryKey = "591DLB6TVP3HC797JIDT"; // Amazon
-//	//public static final String flurryKey = "BWPEYSEM6U823TEWG631"; // Android Market
-	
-	
-	public static final String baseUrl = "http://roemmich-laptop.dyndns.org:3000";
-	public static final String cookieHost = "http://roemmich-laptop.dyndns.org:3000";
+	public static final String baseUrl = "";
+	public static final String cookieHost = "";
 	public static final String oauthUrl = baseUrl + "/oauth";
 	public static final String apiUrl = baseUrl + "/api";
 	public static final String apiVersion = "2";
-	public static final String oauthClientId = "5";
-	public static final String oauthClientSecret = "a47326052ee47060847acc7a8bbdc587f6ba5dac70d2be82b6a1a4761037b3c8";
+	public static final String oauthClientId = "";
+	public static final String oauthClientSecret = "";
 	public static final String oauthScope = "userinfo,contacts,contact_assignment,followup_comments,roles,organization_info";
-	public static final String flurryKey = "GNH51RVF8SGEMQZX5DQT"; // Development
+	public static final String flurryKey = "";
 }
