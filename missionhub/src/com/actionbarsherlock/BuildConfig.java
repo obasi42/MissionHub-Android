4c1058e98382c0cbbd2f771a365c5e20f1dfe442
Chris Roemmich <chris.roemmich@cojourners.com>
2013-08-27 15:41:47 -0500

    fix proguard build

---
+++ missionhub/src/com/actionbarsherlock/BuildConfig.java
@@ -0,0 +1,9 @@
+package com.actionbarsherlock;
+
+/**
+ * Hack while waiting ABS to fix AAR issue.
+ * https://github.com/JakeWharton/ActionBarSherlock/issues/1001
+ **/
+public class BuildConfig {
+    public static final boolean DEBUG = com.missionhub.BuildConfig.DEBUG;
+}
